dc687729335ca00f71ef4854ea8a1a60b0681a72
danimdev <hforgames4@googlemail.com>
2022-06-02 21:02:31 +0200

    Added the FIles to the Repository

---
+++ DocumentationTool/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+GetInput();
+
+void GetInput()
+{
+    
+    string textFileName = "Document " + DateTime.Now.ToString("H m ss") + ".txt";
+    FileStream stream = new FileStream(textFileName, FileMode.OpenOrCreate);
+
+    Console.WriteLine("What do you want to Document?");
+    using (var t = new StreamWriter(stream))
+    {
+        t.AutoFlush = true;
+
+        do
+        {
+            Console.Write("Text: ");
+
+            string inputToFile = Console.ReadLine();
+
+            t.WriteLine(inputToFile);
+
+            Console.Write("Press Enter For A New Line...");
+            Console.WriteLine();
+
+        } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+        t.Dispose();
+        t.Close();
+    }
+}
